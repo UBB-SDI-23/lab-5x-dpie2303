42110268e1325f313807f59a8a87318903500370
dpie2303 <dpie2303@scs.ubbcluj.ro>
2023-04-24 16:17:04 +0300

    enable server

---
+++ install_dependecies.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update package list
+sudo yum -y update
+
+# Install Python 3 and pip
+sudo yum -y install python3 python3-pip
+
+# Install Node.js and npm
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum -y install nodejs
+
+# Install Python libraries
+pip3 install --user django boto3 requests drf_yasg
+
+# Install Node.js libraries
+sudo npm install -g react react-dom axios npm@9.6.5
+
+# Print versions
+python3 --version
+pip3 --version
+node --version
+npm --version
+
+# Print installed Python libraries
+pip3 freeze
