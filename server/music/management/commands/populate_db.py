912bcbf42cd65c0124e5a47c0c0be20ab663cb51
dpie2303 <dpie2303@scs.ubbcluj.ro>
2023-05-12 18:48:37 +0300

    update deployment

--- server/music/management/commands/populate_db.py
+++ server/music/management/commands/populate_db.py
@@ -26,7 +26,9 @@ class Command(BaseCommand):
             return
         # 'drop_indexes.sql', 'record_companies.sql', 'albums.sql', 'artists.sql',
         #scripts = ['drop_duplicates.sql', 'create_indexes.sql']
-        scripts = ['drop_indexes.sql', 'record_companies.sql', 'albums.sql', 'artists.sql', 'tracks.sql', 'track_artist_colabs.sql', 'create_indexes.sql']    
+        
+        scripts = ['drop_indexes.sql', 'record_companies.sql', 'albums.sql', 'artists.sql', 'tracks.sql', 'track_artist_colab.sql' , 'create_indexes.sql', 'drop_duplicates.sql']    
+
 
         for script in scripts:
             self.stdout.write(self.style.SUCCESS(f'Database started populating {script}'))
