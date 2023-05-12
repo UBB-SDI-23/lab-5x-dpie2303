7ec93e188e2b2fc78d37431f576b6de4940bf5d7
dpie2303 <dpie2303@scs.ubbcluj.ro>
2023-05-12 10:47:11 +0300

    modify drop_duplicates

--- server/sql_scripts/drop_duplicates.sql
+++ server/sql_scripts/drop_duplicates.sql
@@ -1,11 +1,14 @@
 BEGIN;
 
-DELETE FROM music_trackartistcolab
-WHERE (track_id, artist_id) IN (
-    SELECT track_id, artist_id
-    FROM music_trackartistcolab
-    GROUP BY track_id, artist_id
-    HAVING COUNT(*) > 1
-);
+-- Step 1: Create a new temporary table with unique rows
+CREATE TABLE music_trackartistcolab_temp AS 
+SELECT DISTINCT track_id, artist_id
+FROM music_trackartistcolab;
+
+-- Step 2: Rename the original table (backup purpose)
+ALTER TABLE music_trackartistcolab RENAME TO music_trackartistcolab_backup;
+
+-- Step 3: Rename the temporary table to the original name
+ALTER TABLE music_trackartistcolab_temp RENAME TO music_trackartistcolab;
 
 COMMIT;
