f3d00db767be696a88592e3c47379b9e18790da3
dpie2303 <dpie2303@scs.ubbcluj.ro>
2023-05-13 12:56:02 +0300

    update config

---
+++ server/sql_scripts/create_count_colum.sql
@@ -0,0 +1,43 @@
+BEGIN;
+UPDATE music_recordcompany
+UPDATE music_album
+SET tracks_count = (
+    SELECT COUNT(*)
+    FROM music_track
+    WHERE music_track.album_id = music_album.id
+)
+COMMIT;
+
+BEGIN;
+
+UPDATE music_artist
+SET collaborations_count = (
+    SELECT COUNT(*)
+    FROM music_trackartistcolab
+    WHERE music_trackartistcolab.artist_id = music_artist.id
+)
+
+COMMIT;
+
+BEGIN;
+
+UPDATE music_recordcompany
+SET albums_count = (
+    SELECT COUNT(*)
+    FROM music_album
+    WHERE music_album.record_company_id = music_recordcompany.id
+)
+
+COMMIT;
+
+
+BEGIN;
+
+UPDATE music_track
+SET collaborations_count = (
+    SELECT COUNT(*)
+    FROM music_trackartistcolab
+    WHERE music_trackartistcolab.track_id = music_track.id
+)
+
+COMMIT;
