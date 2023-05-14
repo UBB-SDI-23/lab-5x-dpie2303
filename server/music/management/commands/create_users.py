3af06c6803538c2f2d2c469507fbad8d2a86e6e3
dpie2303 <dpie2303@scs.ubbcluj.ro>
2023-05-14 20:53:41 +0300

    fix genenerate script

--- server/music/management/commands/create_users.py
+++ server/music/management/commands/create_users.py
@@ -13,18 +13,7 @@ class Command(BaseCommand):
         User = get_user_model()  # Get the current User model
         faker = Faker()
 
-        user = User.objects.get(id=9891)
-
-        # Creating the UserProfile object
-        UserProfile.objects.create(
-            user=user,
-            bio=faker.text(max_nb_chars=100),
-            location=faker.city(),
-            birth_date=faker.date_of_birth(),
-            gender=faker.random_element(elements=('M', 'F')),
-            marital_status=faker.random_element(elements=('S', 'M', 'D', 'W'))
-        )
-
+    
         # Create admin users
         for i in range(1,10):
             user = User.objects.create_user(username=f'admin_{i}', password='Password.123', email=f'admin_{i}@email.com',is_admin=True)
@@ -35,7 +24,7 @@ class Command(BaseCommand):
             user = User.objects.create_user(username=f'moderator_{i}', password='Password.123', email=f'moderator_{i}@email.com',is_moderator=True)
             UserProfile.objects.create(user=user, bio=faker.text(max_nb_chars=100), location=faker.city(), birth_date=faker.date_of_birth(), gender=faker.random_element(elements=('M', 'F')), marital_status=faker.random_element(elements=('S', 'M', 'D', 'W')))
 
-        # # Create regular users
-        # for i in range(9890):
-        #     user = User.objects.create_user(username=f'user_{i}', password='Password.123', email=f'user_{i}@email.com')
-        #     UserProfile.objects.create(user=user, bio=faker.text(max_nb_chars=100), location=faker.city(), birth_date=faker.date_of_birth(), gender=faker.random_element(elements=('M', 'F')), marital_status=faker.random_element(elements=('S', 'M', 'D', 'W')))
+        # Create regular users
+        for i in range(9890):
+            user = User.objects.create_user(username=f'user_{i}', password='Password.123', email=f'user_{i}@email.com')
+            UserProfile.objects.create(user=user, bio=faker.text(max_nb_chars=100), location=faker.city(), birth_date=faker.date_of_birth(), gender=faker.random_element(elements=('M', 'F')), marital_status=faker.random_element(elements=('S', 'M', 'D', 'W')))
